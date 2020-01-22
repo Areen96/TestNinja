85e961ae98b9008ca7d7ad2cbe2085aa3717c69e
areenzyoud <a.zyoud@foothillsolutions.com>
2020-01-22 14:32:19 +0200

    Testing Strings cont

--- TestNinja.UnitTests/HtmlFormatterTests.cs
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -13,7 +13,12 @@ namespace TestNinja.UnitTests
             var result = formatter.FormatAsBold("abc");
             
             //Specific assertion
-            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+            
+            //More general
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
         }
     }
 }
