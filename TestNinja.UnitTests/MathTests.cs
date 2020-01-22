97665d661a879d9931f109c68d1ab9b6ac118ddd
areenzyoud <a.zyoud@foothillsolutions.com>
2020-01-22 13:32:25 +0200

    023 Parameterized Tests

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -22,24 +22,14 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(2,2,2)]
+        public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
         {
-            var result = _math.Max(2, 1);
-            Assert.That(result,Is.EqualTo(2));
+            var result = _math.Max(a, b);
+            Assert.That(result,Is.EqualTo(expectedResult));
         }
         
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
-        {
-            var result = _math.Max(1, 2);
-            Assert.That(result,Is.EqualTo(2));
-        } 
-        
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnsTheSameArgument()
-        {
-            var result = _math.Max(2, 2);
-            Assert.That(result,Is.EqualTo(2));
-        }
     }
 }
