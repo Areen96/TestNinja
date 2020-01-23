eaa23c01f5ce1d764a20685a325ed71c71f2830f
areenzyoud <a.zyoud@foothillsolutions.com>
2020-01-23 14:12:07 +0200

    Exercise- FizzBuzz

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_NumberDivisibleByThreeAndFive_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        
+        [Test]
+        public void GetOutput_NumberDivisibleByThree_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(9);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        
+        [Test]
+        public void GetOutput_NumberDivisibleByFive_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(25);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+    }
+}
